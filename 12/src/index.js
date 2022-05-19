6e8afacf006796079d105f38f4b978658366620a
temuujin-m <temuujintmk@live.com>
2022-05-19 16:53:08 +0900

    webpack initial setup example

---
+++ 12/src/index.js
@@ -0,0 +1,8 @@
+// main.js
+import sumModule from './sum';
+
+console.log(sumModule(1, 2));
+
+// main.js
+import { sum2 } from './sum';
+console.log(sum2(2, 2));
---
+++ 12/src/sum.js
@@ -0,0 +1,11 @@
+// sum.js
+function sum(a, b) {
+    return a + b;
+}
+  
+export default sum;
+
+// sum.js
+export function sum2(a, b) {
+    return a + b;
+  }
